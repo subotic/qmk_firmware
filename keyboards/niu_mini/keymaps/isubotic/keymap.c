36529bb4990230d24d2fc0990173818a636b6860
Ivan Subotic <ibs@subotic.ch>
2019-02-24 10:05:51 +0100

    add shifted layer

--- keyboards/niu_mini/keymaps/isubotic/keymap.c
+++ keyboards/niu_mini/keymaps/isubotic/keymap.c
@@ -9,16 +9,6 @@
 
 #include QMK_KEYBOARD_H
 
-#define ALT_1 LALT(KC_1)
-#define ALT_2 LALT(KC_2)
-#define ALT_3 LALT(KC_3)
-#define ALT_4 LALT(KC_4)
-#define ALT_5 LALT(KC_5)
-#define ALT_6 LALT(KC_6)
-#define ALT_7 LALT(KC_7)
-#define ALT_8 LALT(KC_8)
-#define ALT_9 LALT(KC_9)
-#define ALT_0 LALT(KC_0)
 #define ESCTL LCTL_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     ESCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_BSLS, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
-    KC_CAPS, KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    MO(2),   KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(3),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
 	/* Layer 1
@@ -54,25 +44,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 */
 	[1] = LAYOUT_ortho_4x12(
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______, KC_GRV,  _______, _______, _______, KC_TRNS, _______, _______, _______, KC_QUOT, KC_EQL,  _______,
+    KC_CAPS, KC_GRV,  _______, _______, _______, KC_TRNS, _______, _______, _______, KC_QUOT, KC_EQL,  _______,
     KC_TRNS, KC_COMM, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______,
     RESET,   _______, KC_TRNS, _______, KC_TRNS, _______, _______, _______, KC_SCLN, KC_QUOT, KC_BSLS, _______
 	),
 
-	/* Layer 2
+	/* Layer 2 (shifted numbers)
+	 * ,-----------------------------------------------------------------------------------.
+	 * |      | S1   |  S2  |  S3  |  S4  |  S5  |  S6  |  S7  |  S8  |  S9  |  S0  |      |
+	 * |------+------+------+------+------+-------------+------+------+------+------+------|
+	 * |      |      |      |      |      |      |      |      |      |      |      |      |
+	 * |------+------+------+------+------+------|------+------+------+------+------+------|
+	 * |      |      |      |      |      |      |      |      |      |      |      |      |
+	 * |------+------+------+------+------+------+------+------+------+------+------+------|
+	 * |      |      |      |      |      |      |      |      |      |      |      |      |
+	 * `-----------------------------------------------------------------------------------'
+	 */
+	[2] = LAYOUT_ortho_4x12(
+   _______, S(KC_1),    S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______,
+   _______, S(KC_GRV),  _______, _______, _______, _______, _______, _______, _______, KC_QUOT, KC_EQL,  _______,
+   _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+   KC_TRNS, _______,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ),
+
+	/* Layer 3 (alted numbers)
 	 * ,-----------------------------------------------------------------------------------.
 	 * |      | ALT1 | ALT2 | ALT3 | ALT4 | ALT5 | ALT6 | ALT7 | ALT8 | ALT9 | ALT0 |      |
 	 * |------+------+------+------+------+-------------+------+------+------+------+------|
-	 * |      |      |      |      |      |      | Left | Down |  Up  |Right |      |      |
+	 * |      |      |      |      |      | ALTG | Left | Down |  Up  |Right |      |      |
 	 * |------+------+------+------+------+------|------+------+------+------+------+------|
 	 * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  | F8   |  F9  |  F10 |      |
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
 	 * |      |      |      |      |      |      |      |      |      |  F11 |  F12 |      |
 	 * `-----------------------------------------------------------------------------------'
 	 */
-	[2] = LAYOUT_ortho_4x12(
-   _______, ALT_1,   ALT_2,   ALT_3,   ALT_4,   ALT_5,   ALT_6,   ALT_7,   ALT_8,   ALT_9,   ALT_0,   _______,
-   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+	[3] = LAYOUT_ortho_4x12(
+   _______, A(KC_1), A(KC_2), A(KC_3), A(KC_4), A(KC_5), A(KC_6), A(KC_7), A(KC_8), A(KC_9), A(KC_0), _______,
+   _______, _______, _______, _______, _______, A(KC_G), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
    _______, _______, _______, _______, _______, _______, _______, KC_TRNS, _______, KC_F11,  KC_F12,  _______
   )
